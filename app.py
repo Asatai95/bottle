d17296333e52166c59067d2b1df8463fbbfeb5c3
Taishi Asato <asatai@TaishinoMacBook-puro.local>
2018-08-21 20:12:22 +0900

    test

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import MySQLdb
 import os
 from bottle import route, run, template, static_file, request, redirect, response
 
@@ -16,21 +15,21 @@ def top():
 
     return template('top')
 
-@route("/test")
-def top_db():
-
-    db = heroku_d9c662866ce227f.connect(db='heroku', host='us-cdbr-iron-east-01.cleardb.net', port=3306, user='b4da42a09cc349', passwd='dd235253')
-    con = db.cursor()
-
-    sql = 'select test from test where id = 1'
-    test = con.execute(sql)
-    db.commit()
-    print(sql)
-    print(test)
-
-    result = con.fetchall()
-    print(result)
-
-    return template('test', tests=result)
+# @route("/test")
+# def top_db():
+#
+#     db = heroku_d9c662866ce227f.connect(db='heroku', host='us-cdbr-iron-east-01.cleardb.net', port=3306, user='b4da42a09cc349', passwd='dd235253')
+#     con = db.cursor()
+#
+#     sql = 'select test from test where id = 1'
+#     test = con.execute(sql)
+#     db.commit()
+#     print(sql)
+#     print(test)
+#
+#     result = con.fetchall()
+#     print(result)
+#
+#     return template('test', tests=result)
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
