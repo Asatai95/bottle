93c8cf0b66d8038ba20013e1f8b1f082353e72f0
Taishi Asato <asatai@TaishinoMacBook-puro.local>
2018-08-28 18:41:44 +0900

    test

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def sendmail():
     Subject: %s
 
     %s
-    """ % (sent_form, ", ".join(to).encode('utf_8'), subject.encode('utf_8'), body_sub )
+    """ % (sent_form, ", ".join(to), subject, body_sub )
 
     if email_text is not False:
         server = smtplib.SMTP('smtp.gmail.com', 587)
