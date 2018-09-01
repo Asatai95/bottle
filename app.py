a8b8988fa375354fdbbad2426e3648752e78360a
Taishi Asato <asatai@TaishinoMacBook-puro.local>
2018-09-01 20:22:55 +0900

    test

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def sendmail():
 
         msg = MIMEText(body.encode(jp_encoding), "plain", jp_encoding)
 
-        msg['From'] = Header(sender_name, jp_encoding)
+        msg['From'] = sender_name.encode(jp_encoding)
         From = msg['From']
         msg['Subject'] = Header(mail_subject, jp_encoding)
         msg['To'] = you
