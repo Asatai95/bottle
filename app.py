80310f8d107e23bda3f47644cff94f0e57454a60
Taishi Asato <asatai@TaishinoMacBook-puro.local>
2018-09-04 17:50:04 +0900

    更新

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def text():
 @route('/text', method='POST')
 def text_db():
 
-    form = request.forms('form')
+    form = request.forms ('form')
     print(form)
 
     db = MySQLdb.connect(user='b4da42a09cc349', passwd='dd235253', host='us-cdbr-iron-east-01.cleardb.net', db='heroku_d9c662866ce227f', charset='utf8')
